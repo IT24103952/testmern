68f66536e6b12012f8535539efa3de9ae615183f
kajananan <it24103816@my.sliit.lk>
2026-04-28 16:58:48 +0530

    Updated itemapi

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL:"https://testmern-production-35d7.up.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
